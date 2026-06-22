ea810b397fd3a16b2f55cbd3236190b493fb4bb7
IsaiahNaden <isaiahnaden22@gmail.com>
2026-06-22 21:34:31 +0800

    final code changes

--- hashTableSearch/search/hash_table_search.cpp
+++ hashTableSearch/search/hash_table_search.cpp
@@ -1,3 +1,20 @@
+// *********************************************************
+// Program: hash_table_search.cpp
+// Course: CCP6214 Algorithm Design and Analysis
+// Lecture Class: TC4L
+// Tutorial Class: T13L
+// Trimester: 2610
+// Member_1: 243UC247DQ | Ajjay Naidu a/l Naidu | ajjay.naidu.naidu@student.mmu.edu.my | 012-721 7199
+// Member_2: 243UC247DM | Harvind a/l Sethu Pathy | harvind.sethu.pathy@student.mmu.edu.my | 019-454 6875
+// Member_3: 243UC2466L | Isaiah Naden a/l Felix Arokianathan | isaiah.naden.felix@student.mmu.edu.my | 010-212 2720
+// Member_4: 243UC245LQ | Sanjeevan a/l Rames | SANJEEVAN.RAMES@student.mmu.edu.my | 017- 687 4937
+// *********************************************************
+// Task Distribution
+// Member_1: Q1,Q5,Q6,Q7
+// Member_2: Q3,Q5,Q6,Q7
+// Member_3: Q2,Q5,Q6,Q7 
+// Member_4: Q4,Q5,Q6,Q7
+// *********************************************************
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -132,15 +149,16 @@ vector<Data> parseData(vector<string> arr){
 
 int main(){
 	// open csv file
-	ifstream inputFile("dataset_1000.csv");
+	ifstream inputFile("dataset_100000000.csv");
 	if (!inputFile.is_open()){
 		cerr << "File not found. Please try again.";
 		return 0;
 	}
 	// Get lines from csv file and insert in hash table
 	vector<string> arr = getLines(inputFile);
+	int n = arr.size();
 	vector<Data> finalArr = parseData(arr);
-	HashTable<Data> ht(finalArr.size());
+	HashTable<Data> ht(n);
 	inputFile.close();
 	for(Data d : finalArr){
 		ht.insert(d); 
@@ -185,7 +203,8 @@ int main(){
 	average = average / 1e9;
 	largest = largest / 1e9;	
 
-	ofstream outputFile("hash_table_search_dataset.txt");
+	string fileName = "hash_table_search_dataset_" + to_string(n) + ".txt";
+    ofstream outputFile(fileName);
 	cout << fixed << setprecision(10);
 	outputFile << fixed << setprecision(10);
 	cout << "Best case time: " << smallest << " seconds\n";
